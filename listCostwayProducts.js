aa2e3f9d290fd114155346acd976c3898a664fbb
geofffactorypure <gjarman@factorypure.com>
2026-06-18 15:01:55 -0700

    batch no prog update

--- listCostwayProducts.js
+++ listCostwayProducts.js
@@ -1566,7 +1566,7 @@ async function main() {
             if (!batch) {
                 console.log(`No more parent batches found at index ${batchIndex} (${batchKey}). Done.`)
                 batchIndex++
-
+                batchesWithNoProgress++
                 continue
             }
 
